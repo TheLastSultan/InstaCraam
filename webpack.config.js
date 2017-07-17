08224b275940977c7304bd653ec0a5e4a1685675
TheLastSultan <mozeiny@gmail.com>
2017-07-17 14:27:48 -0700

    Add npm modules and config webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/shootr.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+    modules: [
+      path.resolve(__dirname, 'frontend'),
+      path.resolve(__dirname, 'node_modules')
+    ]
+  }
+};
