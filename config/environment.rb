a57f8621b0a4cec70eab6dfa127653029d8eedb6
TheLastSultan <mozeiny@gmail.com>
2017-07-17 14:09:20 -0700

    Set Jbuilder to format json into lowerCamelCase

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set Jbuilder to format json into lowerCamelCase
+Jbuilder.key_format camelize: :lower
