cf0397483b1bc87c4e2f03d6e9c3d8c7df9c55bb
TheLastSultan <mozeiny@gmail.com>
2017-07-21 15:40:45 -0700

    Add Comments controller, update routes

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,10 @@
+class Api::CommentsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :images, except: [:new, :edit]
+    resources :images, except: [:new, :edit] do
+      resources :comments, only: [:create, :destroy, :show]
+    end
     resources :users, only: [:create, :update, :show] do
       resources :images, only: [:index]
     end
