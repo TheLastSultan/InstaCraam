3ac183f352a3399b2c735174b16c0af3b3616462
TheLastSultan <mozeiny@gmail.com>
2017-07-28 00:06:56 -0700

    Remove console.log

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,7 +1,6 @@
 import values from 'lodash/values';
 import keysIn from 'lodash/keysIn';
 
-
 export const selectAllObjects = objs => (
   values(objs)
 );
@@ -10,26 +9,8 @@ export const selectIds = objs => (
   keysIn(objs).map(el => parseInt(el))
 );
 
-
-
-
-
-
-
-
-
-// export const selectAllObjects = images => (
-//   values(images.byId)
-// );
-
-
-
 export const selectAllComments = (comments) => (
   values(comments.byId)
-);
-
-export const selectCommentsForPost = (comments, postId) => (
-  console.log(values(comments))
 );
 
 export const selectSingleImage = ({ images }, id) => {
