3ea3f6aa6dfde5fbf58172a007edc9204cc59cb7
TheLastSultan <mozeiny@gmail.com>
2017-07-28 00:07:54 -0700

    Remove unused :image validation in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,6 @@ class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :username, uniqueness: true
-  # validates :images, presence: true
 
   after_initialize :ensure_session_token
 
