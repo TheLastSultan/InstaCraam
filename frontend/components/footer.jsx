6a0cb8d3b7e18a057c98b51323c23f12f28ca473
TheLastSultan <mozeiny@gmail.com>
2017-10-26 17:05:03 -0700

    Update footer link

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,12 +5,24 @@ const Footer = () => (
     <div className="footer-container">
       <nav>
         <ul>
-          <li><a href="http://github.com/nrrs" target="_blank">github</a></li>
-          <li><a href="https://www.linkedin.com/in/norriskwan" target="_blank">LinkedIn</a></li>
-          <li><a href="#" target="_blank">Portfolio</a></li>
+          <li>
+            <a href="http://github.com/nrrs" target="_blank">
+              github
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/norriskwan" target="_blank">
+              LinkedIn
+            </a>
+          </li>
         </ul>
       </nav>
-      <div className="shootr-footer">2017 Norris Kwan</div>
+      <div className="shootr-footer">
+        2017
+        <a href="http://www.norriskwan.com" target="_blank">
+          &nbsp;Norris Kwan
+        </a>
+      </div>
     </div>
   </footer>
 );
